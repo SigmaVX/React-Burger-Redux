ca3cdc501acd3a3d770afbcd9819f6e227ab99fc
Tony Wible <TonyWible@Tonys-MacBook-Pro.local>
2019-01-15 19:25:13 -0500

    Updates

---
+++ precache-manifest.753d1d0b86e7b126d6cc316d6d559ba2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fe03e1fd0a079102ae3",
+    "url": "/React-Redux-Burger/static/css/3.7e8d79fa.chunk.css"
+  },
+  {
+    "revision": "90d6d53463b8cebd0cf9",
+    "url": "/React-Redux-Burger/static/css/main.2d8319bc.chunk.css"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-Redux-Burger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "97436a285e3992acad56",
+    "url": "/React-Redux-Burger/static/js/1.97436a28.chunk.js"
+  },
+  {
+    "revision": "563bd8dc823dee36841cf51ec8bcdad1",
+    "url": "/React-Redux-Burger/static/media/BobsBurgers.563bd8dc.jpg"
+  },
+  {
+    "revision": "69c0c6c9cc6e9ec55849",
+    "url": "/React-Redux-Burger/static/js/2.69c0c6c9.chunk.js"
+  },
+  {
+    "revision": "90d6d53463b8cebd0cf9",
+    "url": "/React-Redux-Burger/static/js/main.90d6d534.chunk.js"
+  },
+  {
+    "revision": "0fe03e1fd0a079102ae3",
+    "url": "/React-Redux-Burger/static/js/3.0fe03e1f.chunk.js"
+  },
+  {
+    "revision": "af037c165f2703beecd2",
+    "url": "/React-Redux-Burger/static/js/4.af037c16.chunk.js"
+  },
+  {
+    "revision": "0f47bfcb6843edf8692f",
+    "url": "/React-Redux-Burger/static/js/runtime~main.0f47bfcb.js"
+  },
+  {
+    "revision": "69c0c6c9cc6e9ec55849",
+    "url": "/React-Redux-Burger/static/css/2.4c4e594b.chunk.css"
+  },
+  {
+    "revision": "97436a285e3992acad56",
+    "url": "/React-Redux-Burger/static/css/1.87cbf134.chunk.css"
+  },
+  {
+    "revision": "32d380409b90fba5768f29c342d67151",
+    "url": "/React-Redux-Burger/index.html"
+  }
+];
